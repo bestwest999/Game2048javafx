aa95e4cd274215a748759354be201e19a8f195dd
Denis <stkalash@yandex.ru>
2017-10-05 22:17:25 +0100

    First commit

---
+++ src/app2048/ApplicationView.java
@@ -0,0 +1,34 @@
+
+package  app2048;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+
+
+ public class ApplicationView {
+
+    private SimpleIntegerProperty number = new SimpleIntegerProperty(2);
+
+     public GridPane drawBoard() {
+        Label[] pane = new Label[2];
+        Label a = new Label();
+        Label b = new Label();
+        a.textProperty().bind(number.asString());
+        b.textProperty().bind(number.asString());
+
+
+        pane[0] = a;
+        pane[1] = b;
+        pane[0].setMinSize(100, 100);
+        pane[1].setMinSize(100, 100);
+        pane[0].setVisible(false);
+        GridPane board = new GridPane();
+        board.setMinSize(400,400);
+        board.add(pane[0],0,0);
+        board.add(pane[1],1,0);
+        board.setGridLinesVisible(true);
+        return board;
+    }
+}
+
---
+++ src/app2048/app2048.java
@@ -0,0 +1,19 @@
+package app2048;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class app2048 extends Application {
+
+        public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @Override
+    public void start(Stage stage) {
+        ApplicationView board = new ApplicationView();
+        Scene scene = new Scene(board.drawBoard());
+        stage.setScene(scene);
+        stage.setTitle("Adding Children to a GridPane");
+        stage.show();
+    }
+}
