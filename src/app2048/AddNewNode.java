778115355887bcbd483958978415f0879273d863
Denis Kalashnikov <stkalash@yandex.ru>
2017-10-06 14:21:17 +0100

    first update from laptop

--- src/app2048/AddNewNode.java
+++ src/app2048/AddNewNode.java
@@ -4,17 +4,26 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class AddNewNode {
-    static void shuffleArray(int[] ar)
-    {
 
+    static void addNode(ApplicationView board) {
+        int[] xCoord = shuffleArray();
+        int[] yCoord = shuffleArray();
+
+
+    }
+
+
+    private static int[] shuffleArray() {
         Random rnd = ThreadLocalRandom.current();
-        for (int i = ar.length - 1; i > 0; i--)
-        {
+        int[] ar = {0, 1, 2, 3};
+        for (int i = 3; i > 0; i--) {
             int index = rnd.nextInt(i + 1);
             // Simple swap
+            //dfgdf
             int a = ar[index];
             ar[index] = ar[i];
             ar[i] = a;
         }
+        return ar;
     }
 }
