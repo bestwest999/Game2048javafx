fdd38d690391bacc7f0c6a95e9b4ef1c78d8ff4e
Denis Kalashnikov <stkalash@yandex.ru>
2017-10-06 17:33:16 +0100

    some changes

--- src/app2048/ApplicationView.java
+++ src/app2048/ApplicationView.java
@@ -13,12 +13,7 @@ public class ApplicationView extends GridPane {
 
    public GridPane drawBoard() {
 
-      //addNode();
-     // return board;
-   //}
 
-  // public void addNode() {
-      //GridPane b = new GridPane();
       SimpleIntegerProperty number = new SimpleIntegerProperty(2);
       board.setMinSize(400, 400);
       board.setGridLinesVisible(true);
@@ -29,20 +24,19 @@ public class ApplicationView extends GridPane {
       tile2.textProperty().bind(number.asString());
       tile2.setMinSize(100,100);
       int[] xCoord = shuffleArray();
-      System.out.println("a=" + xCoord[0] + "b=" + xCoord[1] + "c=" + xCoord[2] + "d=" + xCoord[3]);
+      //System.out.println("a=" + xCoord[0] + "b=" + xCoord[1] + "c=" + xCoord[2] + "d=" + xCoord[3]);
       int[] yCoord = shuffleArray();
-      System.out.println("a=" + yCoord[0] + "b=" + yCoord[1] + "c=" + yCoord[2] + "d=" + yCoord[3]);
+      //System.out.println("a=" + yCoord[0] + "b=" + yCoord[1] + "c=" + yCoord[2] + "d=" + yCoord[3]);
       int a = xCoord[0];
       int b = yCoord[1];
       //  for (int x = 3; x > 0; x--) {
       //   for (int y = 3; y > 0; y--) {
-            board.add(tile, a, b);
-            board.add(tile2, xCoord[a], yCoord[b]);
+            board.add(tile, xCoord[1], yCoord[1]);
+            board.add(tile2, xCoord[0], yCoord[0]);
       return board;
 
-        // }
-      //}
-   }}
-//}
+
+   }
+}
 
 
