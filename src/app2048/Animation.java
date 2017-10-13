310233fc8193d99e6f2753bc370d57c9d7344284
Denis Kalashnikov <stkalash@yandex.ru>
2017-10-13 13:09:47 +0100

    friday

--- src/app2048/Animation.java
+++ src/app2048/Animation.java
@@ -1,18 +1,10 @@
 package app2048;
 
-import com.sun.corba.se.impl.orb.ParserTable;
-import com.sun.javafx.geom.BaseBounds;
-import com.sun.javafx.geom.transform.BaseTransform;
-import com.sun.javafx.jmx.MXNodeAlgorithm;
-import com.sun.javafx.jmx.MXNodeAlgorithmContext;
-import com.sun.javafx.sg.prism.NGNode;
+
 import javafx.animation.TranslateTransition;
-import javafx.beans.Observable;
-import javafx.beans.binding.BooleanBinding;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
 import javafx.geometry.Bounds;
@@ -21,8 +13,8 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.util.Duration;
-
 import java.util.Comparator;
+import java.util.Observable;
 
 public class Animation {
 
@@ -30,9 +22,11 @@ public class Animation {
     private ApplicationView view;
 
 
+
     public Animation(ApplicationView applicationView) {
         view = applicationView;
         board = view.getBoard();
+
 
 
     }
@@ -41,59 +35,72 @@ public class Animation {
         GridPane board = this.view.getBoard();
         ObservableList<Node> childrens = board.getChildren();
         ObservableList<Bounds> bounds = FXCollections.observableArrayList();
+
         Comparator<Node> comparator = new Comparator<Node>() {
-            @Override
-            public int compare(Node node1, Node node2) {
-                return ((int) node1.getBoundsInParent().getMinX() - (int) node2.getBoundsInParent().getMinX());
-            }
-        };
+           @Override
+          public int compare(Node node1, Node node2) {
+             return ((int) node1.getBoundsInParent().getMinX() - (int) node2.getBoundsInParent().getMinX());
+        }
+         };
         SortedList<Node> sorted = new SortedList<Node>(childrens, comparator);
+
+
 
         for (Node node : sorted) {
             TranslateTransition tt = new TranslateTransition(Duration.seconds(1), node);
+            // ObservableList<Node> childrens = board.getChildren();
+
             bounds.add(node.getBoundsInParent());
-            node.boundsInParentProperty().addListener(new ChangeListener<Bounds>() {
+
+
+
+
+           node.boundsInParentProperty().addListener(new ChangeListener<Bounds>() {
                 @Override
                 public void changed(ObservableValue<? extends Bounds> observable, Bounds oldValue, Bounds newValue) {
+
+
 
                     if (newValue.getMinX() < 0) {
                         tt.pause();
                         System.out.println("Fuck off");
 
-                    } else if (collisionCheck(sorted, node)) {
+                    } else if (collisionCheck(bounds , node)) {
                         tt.pause();
                         System.out.println("Fuck node");
 
                     }
+                    bounds.remove(oldValue);
+                    bounds.add(newValue);
+
 
                 }
 
             });
-            for (int i = 0; i < 2; i++) {
+
                 tt.setByX(-400);
                 tt.setCycleCount(3);
                 tt.play();
-            }
         }
     }
 
-    public boolean collisionCheck(ObservableList<Node> children, Node node) {
+    public boolean collisionCheck(ObservableList<Bounds> children, Node node) {
 
         //System.out.println("LayoutX = " + node.getBoundsInParent().getMinX());
 
 
-        for (Node tile : children) {
+        for (Bounds tile : children) {
 
-            if (node != tile) if (node.getBoundsInParent().intersects(tile.getBoundsInParent()))
-                if (node.getBoundsInParent().getMaxY() == tile.getBoundsInParent().getMaxY())
-                    if (node.getBoundsInParent().getMinX() > tile.getBoundsInParent().getMinX())
+            if (node.getBoundsInParent() != tile) if (node.getBoundsInParent().intersects(tile))
+                if (node.getBoundsInParent().getMaxY() == tile.getMaxY())
+                    if (node.getBoundsInParent().getMinX() > tile.getMinX())
 
                     {         //     && (node.getLayoutX()!=node.getLayoutX())
                         // System.out.println("LayoutX = " + node.getBoundsInParent() + "LayoutY = " + node.getLayoutY());
                         //System.out.println("LayoutX = " + tile.getBoundsInParent() + "LayoutY = " + node.getLayoutY());
 
                         return true;
-                    } else if (node.getBoundsInParent().getMinX() > tile.getBoundsInParent().getMinX()) {
+                    } else if (node.getBoundsInParent().getMinX() > tile.getMinX()) {
                         System.out.println("bad");
                         return true;
 
@@ -114,7 +121,6 @@ public class Animation {
         String type = e.getEventType().getName();
         KeyCode keyCode = e.getCode();
         System.out.println(type + ": Key Code=" + keyCode.getName() + ", Text=" + e.getText());
-        moveTiles();
         moveTiles();
     }
 
