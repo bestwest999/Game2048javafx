e017ed490be8972eb1a8667dccb3defdbbe5c44c
Denis Kalashnikov <stkalash@yandex.ru>
2017-10-14 23:32:56 +0100

    The win blad

--- src/app2048/Animation.java
+++ src/app2048/Animation.java
@@ -32,10 +32,14 @@ public class Animation {
 
     }
 
+    /*
+     moveLeft() move all present tiles to left
+     @param firstRowDist, secondRowDist, thirdRowDist, fouthRoWDist - distance filled by tilse that are allready moved left
+     */
+
     public void moveLeft() {
         GridPane board = this.view.getBoard();
         ObservableList<Node> childrens = board.getChildren();
-        ObservableList<Bounds> bounds = FXCollections.observableArrayList();
 
         Comparator<Node> comparator = new Comparator<Node>() {
             @Override
@@ -45,161 +49,34 @@ public class Animation {
         };
         SortedList<Node> sorted = new SortedList<Node>(childrens, comparator);
 
+        double firstRowDist = 0;
+        double secondRowDist = 0;
+        double thirdRowDist = 0;
+        double fouthRowDist = 0;
 
         for (Node node : sorted) {
-            TranslateTransition tt = new TranslateTransition(Duration.seconds(2), node);
+            TranslateTransition tt = new TranslateTransition(Duration.seconds(1), node);
 
+            if (node.getBoundsInParent().getMinY() == 0) {
+                tt.setByX(-(node.getBoundsInParent().getMinX()) + firstRowDist);
+                firstRowDist += 100;
+                tt.play();
+            } else if (node.getBoundsInParent().getMinY() == 100) {
 
-            bounds.add(node.getBoundsInParent());
-
-
-            node.boundsInParentProperty().addListener(new ChangeListener<Bounds>() {
-                @Override
-                public void changed(ObservableValue<? extends Bounds> observable, Bounds oldValue, Bounds newValue) {
-                    int x = (int)(newValue.getMinX()/100)*100;
-                    int y = (int)(newValue.getMinY()/100)*100;
-
-                    if (newValue.getMinX() < 0) {
-                        tt.pause();
-
-
-
-                    } else if  (leftCollisionCheck(bounds, node)) {
-
-                        tt.pause();
-
-
-                    }
-
-                    bounds.remove(oldValue);
-                    bounds.add(newValue);
-
-
-                }
-
-            });
-
-tt.setNode(node);
-           tt.setByX(-300);
-           tt.play();
-
-        }
-    }
-
-    public void moveRight() {
-        GridPane board = this.view.getBoard();
-        ObservableList<Node> childrens = board.getChildren();
-        ObservableList<Bounds> bounds = FXCollections.observableArrayList();
-
-        Comparator<Node> comparator = new Comparator<Node>() {
-            @Override
-            public int compare(Node node1, Node node2) {
-                return ((int) node1.getBoundsInParent().getMinX() - (int) node2.getBoundsInParent().getMinX());
+                tt.setByX(-(node.getBoundsInParent().getMinX()) + secondRowDist);
+                secondRowDist += 100;
+                tt.play();
+            } else if (node.getBoundsInParent().getMinY() == 200) {
+                tt.setByX(-(node.getBoundsInParent().getMinX()) + thirdRowDist);
+                thirdRowDist += 100;
+                tt.play();
+            } else if (node.getBoundsInParent().getMinY() == 300) {
+                tt.setByX(-(node.getBoundsInParent().getMinX()) + fouthRowDist);
+                fouthRowDist += 100;
+                tt.play();
             }
-        };
-        SortedList<Node> sorted = new SortedList<Node>(childrens, comparator.reversed());
-
-
-        for (Node node : sorted) {
-            TranslateTransition tt = new TranslateTransition(Duration.seconds(2), node);
-
-            bounds.add(node.getBoundsInParent());
-
-
-            node.boundsInParentProperty().addListener(new ChangeListener<Bounds>() {
-                @Override
-                public void changed(ObservableValue<? extends Bounds> observable, Bounds oldValue, Bounds newValue) {
-
-
-                    if (newValue.getMaxX() > 400) {
-                        tt.pause();
-                        board.requestLayout();
-
-                    } else if (rightCollisionCheck(bounds, node)) {
-                        double x = (int)(newValue.getMinX()/100)*100;
-                        tt.pause();
-                    }
-
-                    double x = (int)(newValue.getMinX()/100)*100;
-                    int y = (int)(newValue.getMinY()/100)*100;
-
-
-                    bounds.remove(oldValue);
-                    bounds.add(newValue);
-
-
-
-                }
-
-            });
-
-            tt.setNode(node);
-    tt.setByX(300);
-    tt.play();
-
         }
     }
-
-
-
-
-
-    public boolean leftCollisionCheck(ObservableList<Bounds> children, Node node) {
-
-       // System.out.println(" before LayoutX = " + node.getBoundsInParent().getMinX());
-
-
-        for (Bounds tile : children) {
-
-            if (node.getBoundsInParent() != tile) if (node.getBoundsInParent().intersects(tile))
-                if (node.getBoundsInParent().getMaxY() == tile.getMaxY())
-                    if (node.getBoundsInParent().getMinX() > tile.getMinX())
-
-                    {
-
-
-                        return true;
-                    } else {
-                        return false;
-
-                    }
-        }
-
-
-        return false;
-    }
-
-
-
-    public boolean rightCollisionCheck(ObservableList<Bounds> children, Node node) {
-
-       // System.out.println(" before LayoutX = " + node.getBoundsInParent().getMinX());
-
-
-        for (Bounds tile : children) {
-
-            if (node.getBoundsInParent() != tile) if (node.getBoundsInParent().intersects(tile))
-                if (node.getBoundsInParent().getMaxY() == tile.getMaxY())
-                    if (node.getBoundsInParent().getMinX() < tile.getMinX())
-
-                    {
-
-
-                        return true;
-                    } else {
-                        return false;
-
-                    }
-        }
-
-
-        return false;
-    }
-
-
-
-    // System.out.println("LayoutX = " + tile.getLayoutX() + "TranslateX = " + tile.getTranslateX() + "board witdth = " + board.getWidth());
-    //System.out.println("getBoundsInParent = " + tile.getBoundsInParent() + "getBoundsInLocal = " + tile.getBoundsInLocal() + "getBoundsInLocal = " + board.getBoundsInLocal().getMaxX());
 
 
     public void handle(KeyEvent e) {
@@ -209,10 +86,10 @@ tt.setNode(node);
         if (e.getCode() == KeyCode.LEFT) {
             moveLeft();
 
-        }
-        else if (e.getCode() == KeyCode.RIGHT) {
-            moveRight();
-        } else{
+        } else if (e.getCode() == KeyCode.RIGHT) {
+            //moveRight();
+            System.out.println("Do nothing");
+        } else {
             System.out.println("Do nothing");
         }
     }
